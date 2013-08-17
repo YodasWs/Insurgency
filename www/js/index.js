fad4da9303a5dded2754874c20dd7ce0886b5302
Sam Grundman <samuel@yodas.ws>
2013-08-17 17:47:50 -0400

    Update index.js
    fix syntax error

--- www/js/index.js
+++ www/js/index.js
@@ -1,24 +1,24 @@
 var ajax = {
-	this.get = function(url, callback) {
-//		console.log('ajax.get looking at ' + url);
-//		var xhr = XMLHttpRequest();
-//		xhr.open('get', url, true);
-//		xhr.send();
-//		xhr.onreadystatechange = function() {
-//			console.log('Ajax response ready state = ' + this.readyState);
-//			if (this.readyState != 4) return false;
+	get: function(url, callback) {
+		console.log('ajax.get looking at ' + url);
+		var xhr = XMLHttpRequest();
+		xhr.open('get', url, true);
+		xhr.send();
+		xhr.onreadystatechange = function() {
+			console.log('Ajax response ready state = ' + this.readyState);
+			if (this.readyState != 4) return false;
 			var data = null;
 			if (this.status == 200) {
-//				var type = this.getResponseHeader('Content-type');
-//				data = this.responseText;
-//				if (type == 'application/json')
-//					data = eval('(' + data + ')');
+				var type = this.getResponseHeader('Content-type');
+				data = this.responseText;
+				if (type == 'application/json')
+					data = eval('(' + data + ')');
 			}
-//			if (typeof callback == 'function')
-//				callback(data);
-//		}
+			if (typeof callback == 'function')
+				callback(data);
+		}
 	},
-	this.post = function(url, data, callback) {
+	post: function(url, data, callback) {
 //		var xhr = XMLHttpRequest();
 //		xhr.open('post', url, true);
 //		xhr.send(data);
@@ -39,15 +39,13 @@ var ajax = {
 };
 
 document.write('<div>hello from index.js</div>');
-console.log('index.js loaded');
 
 (function() {
 	document.addEventListener('deviceready', function() {
-//		console.log('Device Ready =)');
-//		ajax.get('http://fps.yodas.ws/friends', function(data) {
-//			console.log('Response from ajax received');
-//			alert(JSON.stringify(data));
-//		});
-alert('device ready');
+		console.log('Device Ready =)');
+		ajax.get('http://fps.yodas.ws/friends', function(data) {
+			console.log('Response from ajax received');
+			alert(JSON.stringify(data));
+		});
 	}, false);
 })();
