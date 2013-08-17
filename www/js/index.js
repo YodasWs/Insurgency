8b9bdd390cc6381ebcda8c36bd247de9c2bf4c7b
Sam Grundman <samuel@yodas.ws>
2013-08-17 16:31:27 -0400

    Add Ajax to JavaScript

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,27 @@
-﻿(function() {
+var ajax = {
+	this.get = function(url, callback) {
+		this.post(url, null, callback);
+	}
+	this.post = function(url, data, callback) {
+		var xhr = XMLHttpRequest(),
+		xhr.open('post', url, true);
+		xhr.send(data);
+		xhr.onreadystatechange = function() {
+			if (this.readyState != 4) return false;
+			var type = this.getResponseHeader('Content-type');
+			var data = this.responseText;
+			if (type == 'application/json')
+				data = eval('(' + data + ')');
+			if (typeof callback == 'function')
+				callback(data);
+		}
+	}
+};
+
+(function() {
 	document.addEventListener('deviceready', function() {
-		alert("app loaded =)");	
+		ajax.get('http://fps.yodas.ws/friends', function(data) {
+			alert(JSON.stringify(data));
+		});
 	}, false);
 })();
