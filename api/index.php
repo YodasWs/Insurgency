4237b336d08175b38b0ee29c75455ac08acaf9fa
Samuel Grundman <samuel@yodas.ws>
2013-09-06 20:30:13 -0400

    Start RESTful API
    start work on the server-side code of the RESTful API for the app

---
+++ api/index.php
@@ -0,0 +1,16 @@
+﻿<?php
+header('Access-Control-Allow-Origin: *');
+$url = $_SERVER['HTTP_X_REWRITE_URL'];
+$url = substr($url, 4); // remove the temporary '/fps' directory
+if ($url == '/friends') {
+   header('Content-type: application/json');
+   echo json_encode(array(
+      'test' => 'hello world',
+   ));
+   exit;
+}
+# preg_match("'/v[1-9]\d*(\.\d+)?/'", $url, $matches); // to grab and redirect to a specific version of the API
+header('Content-type: text/html');
+?>
+<h1>Hello World</h1>
+<div>More content to come</div>
