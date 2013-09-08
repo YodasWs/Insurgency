7e69b72ee4c30f3406913403acb60428ab2e8e42
Samuel Grundman <samuel@yodas.ws>
2013-09-08 09:13:56 -0400

    Start Building App Navigation
    Some work on the app navigation animations and styling.
    I'm trying to keep the markup appropriate for Windows Phone navigation,
    too...

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,49 @@
-﻿window.onerror = function(error) { console.log(error); };
+﻿// for desktop testing
+window.onerror = function(error) { console.log(error); };
+if (device.platform == 'Chrome') device.platform = 'iOS';
 
 $(document).ready(function() {
+	// TODO: Add iOS Back Button to Headers :-\
+	if (device.platform == 'iOS') {
+		$('article#home > section[data-role="content"]:not(:nth-child(1)) > header[data-role="header"]').prepend(
+			'<a href="#home" data-role="button" data-icon="back" data-inline="true">Back</a>'
+		);
+		$('article#home > section[data-role="content"]:not(:nth-child(1)) a[data-role="button"]').buttonMarkup('refresh');
+	}
+
 	// TODO: Change "Page"
 	$('a[href^="#"]').click(function() {
 		$this = $(this);
+//		if ($this.closest('article').is('#home')) // For phones with back buttons
+//			history.pushState({page:$this.attr('href')}, null, $this.attr('href').substring(1));
+	if ($($this.attr('href')).closest('article').attr('id') == 'home') {
+		// Slide to New Section
+		var oldSection = $this.closest('section');
+		var newSection = $($this.attr('href'));
+		if ($this.attr('href') == '#home') newSection = $('#home > section[data-role="content"]:first');
+		oldSection.css({
+			position:'absolute',top:oldSection.offset().top+'px'
+		});
+		newSection.css({
+			position:'absolute',top:$(window).height()+'px'
+		}).show();
+		$('body').css('overflow','hidden');
+		oldSection.animate({
+			top:'-'+$(window).height()+'px'
+		}, 'slow', function() {
+			$(this).hide();
+		});
+		newSection.animate({
+			top:'0px'
+		}, 'slow', function() {
+			$(this).css({position:''});
+			oldSection.hide();
+			$('body').css('overflow','');
+		});
+		return false;
+	}
 	switch(device.platform) {
 	case 'iOS': case 'Android':
-	case 'Chrome':
 		$('article:visible').fadeOut('slow', function() {
 			$($this.attr('href')).fadeIn('slow');
 		});
@@ -24,9 +61,14 @@ switch(device.platform) {
 case 'WinCE': case 'Win32NT':
 	$('link').first().after('<link rel="stylesheet" href="css/windowsphone.css"/>');
 	break;
-case 'iOS': case 'Android':
-case 'Chrome': // for desktop testing
+case 'iOS':
+	$('a[data-role="button"][data-icon="back"]').attr('data-theme', 'b').buttonMarkup('refresh');
+	$('link').last().after('<link rel="stylesheet" href="css/ios.css"/>');
+	$('link').first().before('<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css"/>');
+	break;
+case 'Android':
 default:
+	$('link').last().after('<link rel="stylesheet" href="css/android.css"/>');
 	$('link').first().before('<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css"/>');
 	break;
 }
