fda6726234c3d23823d64a8c29292ffd25ebf3d8
Samuel Grundman <samuel@yodas.ws>
2013-10-26 13:02:30 -0400

    Zepto name change
    $ is shorter

--- www/js/app.js
+++ www/js/app.js
@@ -1,18 +1,18 @@
-﻿document.addEventListener('deviceready', function() {
+﻿document.addEventListener('deviceready', function() { $ = Zepto;
 	// Load device-specific styling
 	switch(device.platform) {
 	case 'WinCE': case 'Win32NT':
-		Zepto('link').first().after('<link rel="stylesheet" href="css/windowsphone.css"/>');
+		$('link').first().after('<link rel="stylesheet" href="css/windowsphone.css"/>');
 		break;
 	case 'iOS':
-		Zepto('link').last().after('<link rel="stylesheet" href="css/ios.css"/>');
+		$('link').last().after('<link rel="stylesheet" href="css/ios.css"/>');
 		break;
 	case 'Android':
 	default:
-		Zepto('link').last().after('<link rel="stylesheet" href="css/android.css"/>');
+		$('link').last().after('<link rel="stylesheet" href="css/android.css"/>');
 		break;
 	}
 
 	// Add Cross-Device Bootstrap Styling
-	Zepto('input[type="submit"]').addClass('btn').addClass('btn-default');
+	$('input[type="submit"]').addClass('btn').addClass('btn-default');
 });
